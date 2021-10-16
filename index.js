e0e90aff0368ce6868bd21890aecf507aa39fa2b
glazko-pol96 <57094984+glazko-pol96@users.noreply.github.com>
2021-10-16 17:44:25 +0300

    Add files

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const expressHbrs = require('express-handlebars');
+const routes = require('./routes/routes');
+const PORT = 4000
+
+const app = express()
+const hbs = expressHbrs.create({
+  defaultLayout: 'index',
+  extname: 'hbs'
+});
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+app.use(express.static('public'));
+
+app.use(routes);
+
+app.listen(PORT, () => {
+  console.log(`App listening at http://localhost:${PORT}`)
+})
+
